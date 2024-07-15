b259394415ed7a1b581cb5218a8d290306d62532
shaden-s <ebnazmul06@gmail.com>
2024-07-16 00:34:44 +0100

    0.36

--- scripts.js
+++ scripts.js
@@ -49,7 +49,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
      // Open CV Popup
      cvButton.addEventListener('click', function() {
-        cvIframe.src = 'your-cv.pdf'; // Replace with your CV PDF URL
+        cvIframe.src = 'CV.pdf'; // Replace with your CV PDF URL
         cvPopup.style.display = 'flex';
     });
 
